1e5664dc1d2f4c2f7a3aceb7fedbf63e185ec68e
Olgae <olgaems315@gmail.com>
2021-08-04 11:58:05 -0300

    metodos del backend creados

--- controllers/student.js
+++ controllers/student.js
@@ -1,16 +1,143 @@
 'use strict'
+var fs = require('fs');
+var Student = require('../models/student');
 
 var controller = {
-    home: function(req, res){
+    home: function (req, res) {
         return res.status(200).send({
             message: "soy la home"
         });
     },
 
-    test: function(req, res){
+    test: function (req, res) {
         return res.status(200).send({
             message: "Soy el metodo test del controlador"
         });
     },
+
+    SaveStudent: function (req, res) {
+        var student = new Student();
+        var params = req.body;
+        student.name = params.name;
+        student.Real_name = params.Real_name;
+        student.location = params.location;
+        student.langs = params.langs;
+        student.age = params.age;
+        student.level = params.level;
+        student.platform = params.platform;
+        student.motives = params.motives;
+        student.goals = params.goals;
+        student.interests = params.interests;
+        student.music = params.music;
+        student.obs = params.obs;
+        student.image = params.image;
+
+        // saving in DB
+        student.save((err, studentStored) => {
+            if (err) return res.status(200).send({
+                message: 'error al guardar'
+            });
+            if (!studentStored) return res.status(404).send({
+                nessage: 'no se ha podido guardar'
+            });
+            return res.status(200).send({
+                student: studentStored
+            });
+        });
+
+    },
+
+    getStudent: function (req, res) {
+        var studentId = req.params.id;
+        Student.findById(studentId, (err, student) => {
+            if (err) return res.status(500).send(
+                { message: 'Error al devolver datos' }
+            );
+            if (!student) return res.status(404).send(
+                { message: 'El estudiante no existe' }
+            );
+            if (studentId == null) return res.status(404).send(
+                { message: 'el estudiante esta null' }
+            );
+            return res.status(200).send({ student });
+        });
+    },
+
+    getStudents: function (req, res) {
+        Student.find({}).exec((err, students) => {
+            if (err) return res.status(500).send({
+                message: 'error al devolver los datos'
+            });
+            if (!students) return res.status(404).send({
+                message: 'no hay estudiantes'
+            });
+            return res.status(200).send({ students });
+        });
+    },
+
+    updateStudent: function (req, res) {
+        var studentId = req.params.id;
+        var update = req.body;
+
+        Student.findByIdAndUpdate(studentId, update, { new: true }, (err, studentUpdated) => {
+            if (err) return res.status(500).send({
+                message: 'error al actualizar los datos'
+            });
+            if (!studentUpdated) return res.status(404).send({
+                message: 'no existe el estudiante'
+            });
+            return res.status(200).send({ student: studentUpdated });
+        });
+    },
+
+    deleteStudent: function (req, res) {
+        var studentId = req.params.id;
+        Student.findByIdAndRemove(studentId, (err, studentDeleted) => {
+            if (err) return res.status(500).send({ message: 'no se ha podido encontrar el estudnante' });
+            if (!studentDeleted) return res.status(404).send({ message: 'no se pudo eliminar el estudiante' });
+            return res.status(200).send({
+                student: studentDeleted
+            });
+        });
+    },
+
+    uploadImage: function (req, res) {
+        var studentId = req.params.id;
+        var fileName = 'Imagen no subida';
+
+        if (req.files) {
+
+
+            var filePath = req.files.image.path;
+            var fileSplit = filePath.split('\\');
+            var fileName = fileSplit[1];
+            var extSplit = fileName.split('\.');
+            var fileExt = extSplit[1];
+
+            if (fileExt == 'png' || fileExt == 'jpg' || fileExt == 'jpeg' || fileExt == 'gif') {
+                Student.findByIdAndUpdate(studentId, { image: fileName }, { new: true }, (err, studentUpdated) => {
+                    if (err) return res.status(500).send({ message: 'la imagen no se subio' });
+                    if (!studentUpdated) return res.status(404).send({ message: 'la imagen no existe' });
+                    return res.status(200).send({
+                        student: studentUpdated
+                    });
+                });
+            }else{
+                fs.unlink(filePath, (err)=>{
+                    return res.status(200).send({
+                        message: 'la extension no es valida'
+                    });
+                });
+            }
+        } else {
+            return res.status(200).send({
+                message: fileName
+            });
+        }
+    }
+
+
+
 };
-  module.exports = controller;
+
+module.exports = controller;
--- routes/student.js
+++ routes/student.js
@@ -2,8 +2,18 @@
 
 var express = require('express');
 var StudentController = require('../controllers/student');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart({uploadDir: './uploads'}); 
 var router = express.Router();
+
 router.get('/home', StudentController.home);
 router.post('/test', StudentController.test);
+router.post('/save-student', StudentController.SaveStudent);
+router.get('/get-student/:id?', StudentController.getStudent);
+router.get('/get-students/', StudentController.getStudents);
+router.put('/student/:id', StudentController.updateStudent);
+router.delete('/student/:id', StudentController.deleteStudent);
+router.post('/upload-image/:id', multipartMiddleware, StudentController.uploadImage);
+
 
 module.exports = router;
