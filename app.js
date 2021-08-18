db85928e2fbe7304afe33ba9128a10f84eed4b5c
Olgae <olgaems315@gmail.com>
2021-08-18 10:49:53 -0300

    CORS agregado a app.js

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ var bodyParser = require('body-parser');
 var app = express();
 var student_routes = require('./routes/student');
 //middlewares
-
-
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+//CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 
 //rutas
